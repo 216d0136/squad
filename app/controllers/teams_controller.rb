8a0f25d3bec44a8d93281047554c19ad34ab3101
vagrant <vagrant@localhost.localdomain>
2020-07-13 12:01:59 +0000

    [finish] team_post

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,47 @@
 class TeamsController < ApplicationController
+def index
+    @teams = Team.all
+    @team = Team.new
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+  def edit
+    @team = Team.find(params[:id])
+    #screen_user(@team)
+  end
+
+  def create
+    @team = Team.new(team_params)
+    #binding.pry
+     @team.user_id = current_user.id
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      @team = Team.all
+      render 'index'
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to @team
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name, :body, :image)
+    end	
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,23 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   #before_action :screen_user, only: [:edit, :update]
-  def show
+ def show
     @user = User.find(params[:id])
+    @teams = @user.teams
+    @team = Team.new
   end
 
   def index
     @users = User.all
+    @team = Team.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def destroy
-  end
-
-
-
   private
     def user_params
-       params.require(:user).permit(:name, :introduction, :profile_image)
+       params.require(:user).permit(:name, :introduction, :profile_image_id)
     end
 end

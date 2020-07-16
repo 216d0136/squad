6fc036801f6167da7e785983f18d491c7bbebd42
vagrant <vagrant@localhost.localdomain>
2020-07-16 10:21:50 +0000

    [finish] mypage, [add] test_deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
     #sign in後のredirect先変更
     def after_sign_in_path_for(resource)
-      users_path(resource)
+      user_path(resource)
     end
 
     #sign out後のredirect先変更
